a36073d2bf0750c21cb743e28675ba9744de7d9d
Aniitaa <reyanita96@gmail.com>
2025-09-02 18:16:53 -0300

    Agregue evento

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const contenedor = document.querySelector("div");
+  contenedor.addEventListener("click", function () {
+    alert("Hola! Soy el Div");
+  });
+});
