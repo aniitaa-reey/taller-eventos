630692341c95c657768d3ab52c60cffe5b45980f
Aniitaa <reyanita96@gmail.com>
2025-09-02 18:39:54 -0300

    Solucion-Click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const contenedor = document.querySelector("div");
-  contenedor.addEventListener("click", function () {
-    alert("Hola! Soy el Div");
+  contenedor.addEventListener("click", function (event) {
+    if (event.target === contenedor) {
+      alert("Hola! Soy el Div");
+    }
   });
 });
